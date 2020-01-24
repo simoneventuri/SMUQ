beb5b35287543ba88381f04778643b70e71681e9
rstkwsk2 <rstkwsk2@illinois.edu>
2020-01-24 11:13:51 -0600

    Added introductory terminal messages to convey that the code is running

--- src/main/Main.f90
+++ src/main/Main.f90
@@ -26,10 +26,36 @@ character(:), allocatable                                             ::    Sect
 character(:), allocatable                                             ::    SMUQTask
 logical                                                               ::    VarL0D
 
+write(*,*)
+write(*,'(A)') "|=================================================================================|"
+write(*,'(A)') "|                 __________                          _______                     |"
+write(*,'(A)') "|                /             /|    /| |          | |       \                    |"             
+write(*,'(A)') "|               /             / |   / | |          | |        \                   |"
+write(*,'(A)') "|              /             /  |  /  | |          | |         \                  |"
+write(*,'(A)') "|             (_________    /   | /   | |          | |          \                 |"
+write(*,'(A)') "|                       )  /    |/    | |          | |           \                |"
+write(*,'(A)') "|                      /  /           | |          | |            \               |"
+write(*,'(A)') "|                     /  /            | |          | |        \    \              |"
+write(*,'(A)') "|          __________/  /             | l__________| l_________\___!              |"
+write(*,'(A)') "|                                                               \                 |"
+write(*,'(A)') "|                                                                                 |"
+write(*,'(A)') "|=================================================================================|"
+write(*,'(A)') "|                                                                                 |"
+write(*,'(A)') "| By                                                                              |"
+write(*,'(A)') "|     Przemyslaw Rostkowski (1)                                                   |"
+write(*,'(A)') "|     Simone Venturi (1)                                                          |"
+write(*,'(A)') "|                                                                                 |"
+write(*,'(A)') "| (1) University of Illinois at Urbana Champaign                                  |"
+write(*,'(A)') "|                                                                                 |"
+write(*,'(A)') "|=================================================================================|"
+write(*,*)
 !!--------------------------------------------------------------------------------------------------------------------------------
 !! Getting Running DIrectory
 !!--------------------------------------------------------------------------------------------------------------------------------
 call GetCurrentDirectoryPath( Path=RunDirectory )
+write(*,'(A)') 'Running SMUQ in :'
+write(*,'(A)') '  ' // RunDirectory
+write(*,*)
 
 !!--------------------------------------------------------------------------------------------------------------------------------
 !! Reading in command line arguments
@@ -42,6 +68,8 @@ call ProgramDefs%Construct( Input=CMDArgsSection, Prefix=RunDirectory )
 !!--------------------------------------------------------------------------------------------------------------------------------                                                                             , 
 !! Setting up run environment
 !!--------------------------------------------------------------------------------------------------------------------------------
+write(*,'(A)') 'Setting up the run environment'
+write(*,*)
 if ( len_trim(ProgramDefs%GetSuppliedCaseDir()) /= 0 ) then
   FileName = ProgramDefs%GetSuppliedCaseDir() // ProgramDefs%GetInputFilePrefix() // ProgramDefs%GetInputFileSuffix()
   inquire( File=FileName, Exist=VarL0D )
@@ -53,21 +81,33 @@ else
                                         'supplied', ProcName=ProcName )
 end if
 
+write(*,'(A)') '  Creating the output directory : ' 
+write(*,'(A)') '    ' // ProgramDefs%GetOutputDir()
 call MakeDirectory( Path=ProgramDefs%GetOutputDir(), Options='-p' )
 call RemoveDirectory( Path=ProgramDefs%GetOutputDir(), ContentsOnly=.true. )
 
+write(*,'(A)') '  Creating the log directory : ' 
+write(*,'(A)') '    ' // ProgramDefs%GetLogDir()
 call MakeDirectory( Path=ProgramDefs%GetLogDir(), Options='-p' )
 call RemoveDirectory( Path=ProgramDefs%GetLogDir(), ContentsOnly=.true. )
 
+write(*,'(A)') '  Creating the restart directory : ' 
+write(*,'(A)') '    ' // ProgramDefs%GetRestartDir()
 call MakeDirectory( Path=ProgramDefs%GetRestartDir(), Options='-p' )
 call RemoveDirectory( Path=ProgramDefs%GetRestartDir(), ContentsOnly=.true. )
 
 call MakeDirectory( Path=ProgramDefs%GetCaseDir(), Options='-p' )
 
 if ( len_trim(ProgramDefs%GetSuppliedCaseDir()) /= 0 ) then
+  write(*,'(A)') '  Copying over to a local case directory the supplied external case directory : ' 
+  write(*,'(A)') '    ' // ProgramDefs%GetSuppliedCaseDir()
   call RemoveDirectory( Path=ProgramDefs%GetCaseDir(), ContentsOnly=.true. )
   call CopyDirectory( Source=ProgramDefs%GetSuppliedCaseDir(), Destination=ProgramDefs%GetCaseDir(), ContentsOnly=.true. )
 end if
+
+write(*,*)
+write(*,'(A)') 'Run environment set-up complete'
+write(*,*)
 
 !!--------------------------------------------------------------------------------------------------------------------------------
 !! Initializing logger with log file
@@ -78,6 +118,8 @@ call Logger%Initialize( FileName=ProgramDefs%GetLogFilePath(), Status='REPLACE',
 !!--------------------------------------------------------------------------------------------------------------------------------
 !! Reading in input
 !!--------------------------------------------------------------------------------------------------------------------------------
+write(*,'(A)') 'Reading in input'
+write(*,*)
 FileName = ProgramDefs%GetInputFilePath()
 call Input%Read( FileName=FileName )
 
@@ -88,10 +130,15 @@ SMUQTask = Input%GetName()
 !!--------------------------------------------------------------------------------------------------------------------------------
 !! Constructing from input and running analysis
 !!--------------------------------------------------------------------------------------------------------------------------------
+
 select case ( LowerCase(SMUQTask) )
   case('main')
+    write(*,'(A)') 'Constructing objects from input and creating necessary temporary work directories'
+    write(*,*)
     SectionChain = 'main'
     call Root%Construct( Input=Input, SectionChain=SectionChain, Prefix=ProgramDefs%GetCaseDir() )
+    write(*,'(A)') 'Running main analysis'
+
     call RestartUtility%Construct( Input=Root%GetInput(MainSectionName='main', Prefix=ProgramDefs%GetRestartDir(),                &
                                                                           Directory='/main'), Prefix=ProgramDefs%GetRestartDir() )
     call Root%Run()   
